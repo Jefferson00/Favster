faf26d1da4b9e3711e83e39d65a6707f013a4643
Jefferson00 <jeffersonts00@gmail.com>
2021-08-26 01:27:20 -0300

    refactor: update tracks component

--- src/components/Tracks/index.tsx
+++ src/components/Tracks/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import { usePlayer } from "../../contexts/PlayerContext";
 
 import { fadeInUp, stagger } from "../../styles/animations";
@@ -7,6 +8,8 @@ import styles from './tracks.module.scss';
 import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
 import { Rating } from "../Rating";
+import { useAuth } from "../../contexts/AuthContext";
+import { database } from "../../services/firebase";
 
 type Track = {
   id: string;
@@ -28,9 +31,9 @@ export function Tracks({ artistTracks, listType }: TracksProps) {
   const controls = useAnimation();
   const { playList } = usePlayer();
   const { ref, inView } = useInView();
-  const [showingAllTracks, setShowingAllTracks] = useState(false);
-  const [isItemClicked, setIsItemClicked] = useState(false);
+  const { user } = useAuth();
 
+  const [showingAllTracks, setShowingAllTracks] = useState(false);
   const [tracks, setTracks] = useState<Track[]>([]);
 
   useEffect(() => {
@@ -41,9 +44,33 @@ export function Tracks({ artistTracks, listType }: TracksProps) {
     }
   }, [artistTracks, showingAllTracks]);
 
-
   function toggleShowAllTracks() {
     setShowingAllTracks(!showingAllTracks);
+  }
+
+  /**
+   * Change de rating value for an favorite track
+   * @param track : Track
+   * @param value : number
+   */
+  async function handleChangeRating(track: Track, value: number, index: number) {
+    const id = track.id.replace('.', '');
+
+    const trackRef = database.ref(`libs/${user?.id}/tracks/${id}`);
+
+    await trackRef.update({ 'rating': value });
+
+
+    setTracks([...tracks].map((track, idx) => {
+      if (idx === index) {
+        return {
+          ...track,
+          rating: value
+        }
+      } else {
+        return track;
+      }
+    }));
   }
 
   useEffect(() => {
@@ -75,11 +102,24 @@ export function Tracks({ artistTracks, listType }: TracksProps) {
               {listType === 'library' &&
                 <Rating
                   value={track.rating}
+                  onChange={(value) => handleChangeRating(track, value, index)}
                 />
               }
               <div className={styles.imageContainer}>
                 {track.image ?
-                  <img src={track.image} alt={track.title} />
+                  <motion.div
+                    whileHover={{ y: -5 }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    <Image
+                      src={track.image}
+                      objectFit="cover"
+                      width={138}
+                      height={138}
+                      placeholder="blur"
+                      blurDataURL={track.image}
+                    />
+                  </motion.div>
                   :
                   <img src="default.png" alt={track.title} />
                 }
